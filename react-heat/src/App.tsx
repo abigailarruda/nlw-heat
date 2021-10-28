6e970eee80f86146495689ab243aa94df37e8fff
Abigail <itssabbie@gmail.com>
2021-10-28 18:11:10 -0300

    estilização com scss modules

---
+++ react-heat/src/App.tsx
@@ -0,0 +1,13 @@
+import { MessageList } from './components/MessageList';
+import { LoginBox } from './components/LoginBox';
+
+import styles from './App.module.scss';
+
+export function App() {
+  return (
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
+  );
+}
